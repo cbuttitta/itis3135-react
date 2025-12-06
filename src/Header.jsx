d2c0cf48900ca9ce467350b7c3e02470f8dd9bb9
case-buttitta <cbuttitt@charlotte.edu>
2025-12-06 18:03:38 -0500

    fixed header

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,37 +2,36 @@ export default function Header(){
     return <header>
         <h1>Case Buttitta</h1>
         <nav>
-            <a href="../">Home </a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/">Home </a>
             ||
-            <a href="../itis3135/stuff/MY start p@Ge.htm">CRAPPYWebpage</a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/itis3135/stuff/MY start p@Ge.htm">CRAPPYWebpage</a>
             ||
-            <a href="introduction.html">Introduction</a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/curious_bison/about.html">CuriousBison</a>
             ||
-            <a href="introduction.html">Introduction From JSON</a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/website_evaluations.html">Website Evaluations</a>
             ||
-            <a href="contract.html">Contract</a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/fccfsjs_outline.html">FCC Full Stack JavaScript Outline</a>
             ||
-            <a href="../curious_bison/about.html">CuriousBison</a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/itis3135/hobby/index.html">Hobby Page</a>
             ||
-            <a href="website_evaluations.html">Website Evaluations</a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/itis3135/intro_form.html">Intro Form</a>
             ||
-            <a href="fccfsjs_outline.html">FCC Full Stack JavaScript Outline</a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/itis3135/project_overview.html">Project Overview</a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/itis3135/project/index.html">Project Page</a>
             ||
-            <a href="../itis3135/hobby/index.html">Hobby Page</a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/itis3135/review1.html">TBlackford Project Page Review</a>
             ||
-            <a href="../itis3135/intro_form.html">Intro Form</a>
-            ||
-            <a href="../itis3135/project_overview.html">Project Overview</a>
+            <a href="https://webpages.charlotte.edu/cbuttitt/itis3135/itis3135/review2.html">RLaCroix Project Page Review</a>
             ||
             <a href="/">Index</a>
             ||
             <a href="/Introduction">Introduction</a>
             ||
+            <a href="/Contract">Contract</a>
+            ||
             <a href="/IntroductionFromJson">IntroductionFromJson</a>
             ||
             <a href="/IntroductionFilterFromJson">IntroductionFromFilterJson</a>
-            
-
         </nav>
     </header>;
 }
